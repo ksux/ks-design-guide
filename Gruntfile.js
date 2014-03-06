9f3c35841e64e5f5674733040d13a986ade35f05
Chris Basham <csbasham@iu.edu>
2014-03-06 10:07:30 -0500

    First init.

---
+++ Gruntfile.js
@@ -0,0 +1,323 @@
+// Generated using generator-propeller 0.5.6
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Code syntax highlighting.
+  var hljs = require('highlight.js');
+  hljs.configure({ classPrefix: '' });
+
+  grunt.initConfig({
+
+    // Project settings
+    yeoman: {
+      // configurable paths
+      app: require('./bower.json').appPath || 'app',
+      dist: 'dist'
+    },
+
+    // define variables for Gruntfile
+    config: {
+      src:              'src',
+      dist:             'dist',
+      appFolder:        'app',
+      componentsFolder: 'app/bower_components',
+      jsFolder:         'app/scripts',
+      cssFolder:        'app/styles',
+      imgFolder:        'app/img',
+      dataFolder:       'app/data',
+      fontsFolder:      'app/fonts',
+      cssMainFile:      'main',
+      jsMainFile:       'scripts'
+    },
+
+    // watch for file changes and run tasks in response
+    watch: {
+      js: {
+        files: ['<%= config.src %>/<%= config.jsFolder %>/{,*/}*.js'],
+        tasks: ['newer:jshint', 'newer:copy:dist'],
+        options: {
+          livereload: true
+        }
+      },
+      less: {
+        files: ['<%= config.src %>/<%= config.cssFolder %>/**/*.{css,less}'],
+        tasks: ['less', 'newer:assemble']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      pages: {
+        files: ['<%= config.src %>/assemble/pages/{,*/}*.hbs'],
+        tasks: ['newer:assemble']
+      },
+      assemble: {
+        files: ['<%= config.src %>/assemble/{helpers,layouts,partials}/{,*/}*.{hbs,yml,json,js}'],
+        tasks: ['assemble']
+      },
+      data: {
+        files: ['<%= config.src %>/<%= config.dataFolder %>/{,*/}*.csv'],
+        tasks: ['newer:convert', 'newer:assemble', 'newer:copy:dist']
+      },
+      img: {
+        files: ['<%= config.src %>/<%= config.imgFolder %>/{,*/}*'],
+        tasks: ['newer:copy:dist']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/<%= config.cssFolder %>/{,*/}*.css',
+          '<%= config.dist %>/<%= config.jsFolder %>/{,*/}*.js',
+          '<%= config.dist %>/<%= config.fontsFolder %>/{,*/}*',
+          '<%= config.dist %>/<%= config.dataFolder %>/{,*/}*.csv',
+          '<%= config.dist %>/<%= config.imgFolder %>/{,*/}*'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= config.dist %>'
+        }
+      }
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish'),
+        force: true
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.src %>/<%= config.jsFolder %>/{,*/}*.js'
+      ],
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= yeoman.dist %>/.git*',
+            '<%= config.src %>/assemble/<%= config.dataFolder %>/*.json'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.src %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt,xml}',
+            '.htaccess',
+            '*.html',
+            'views/{,*/}*.html',
+            '<%= config.componentsFolder %>/**/*',
+            '<%= config.imgFolder %>/{,*/}*',
+            '<%= config.fontsFolder %>/{,*/}*',
+            '<%= config.dataFolder %>/{,*/}*',
+            '<%= config.imgFolder %>/{,*/}*',
+            '<%= config.jsFolder %>/{,*/}*.js'
+          ]
+        }]
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    // concurrent: {
+    //   server: [
+    //     'copy:styles'
+    //   ],
+    //   test: [
+    //     'copy:styles'
+    //   ],
+    //   dist: [
+    //     'copy:styles',
+    //     'imagemin',
+    //     'svgmin'
+    //   ]
+    // },
+
+    // concat all javascripts into a single file
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['<%= config.src %>/<%= config.jsFolder %>/{,*/}*.js'],
+        dest: '<%= config.dist %>/<%= config.jsFolder %>/<%= config.jsMainFile %>.js'
+      }
+    },
+
+    // compress concatenated javascripts
+    uglify: {
+      options: {
+        banner: '/*! <%= config.jsMainFile %>.js (generated <%= grunt.template.today("dd-mm-yyyy") %>) */\n'
+      },
+      dist: {
+        files: {
+          '<%= config.dist %>/<%= config.jsFolder %>/<%= config.jsMainFile %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    // compile all non-partial LESS files into CSS
+    // and copy all CSS files into their appropriate location as well
+    less: {
+      development: {
+        options: {
+          compress: false
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.src %>/<%= config.cssFolder %>/',
+            src: ['**/*.{less,css}', '!_*'],
+            dest: '<%= config.dist %>/<%= config.cssFolder %>/',
+            ext: '.css'
+          }
+        ]
+      },
+      production: {
+        options: {
+          compress: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.src %>/<%= config.cssFolder %>/',
+            src: ['**/*.{less,css}', '!_*'],
+            dest: '<%= config.dist %>/<%= config.cssFolder %>/',
+            ext: '.min.css'
+          }
+        ]
+      }
+    },
+
+    // Use Assemble to generate all HTML pages
+    assemble: {
+      options: {
+        plugins: ['assemble-contrib-permalinks'],
+        helpers: ['<%= config.src %>/assemble/helpers/{,*/}*.js'],
+        layout: 'default.hbs',
+        layoutdir: '<%= config.src %>/assemble/layouts',
+        partials: '<%= config.src %>/assemble/partials/{,*/}*.hbs',
+        data: '<%= config.src %>/assemble/data/{,*/}*.{json,yml}',
+        flatten: true,
+        //permalinks: {
+        //  preset: 'pretty'
+        //}
+        // postprocess: require('pretty')
+        marked: {
+          breaks: false,
+          gfm: true,
+          highlight: function (code, lang) {
+            return hljs.highlight(lang, code).value;
+          },
+          langPrefix: 'language-',
+          pedantic: false,
+          sanitize: false,
+          silent: false,
+          smartLists: false,
+          smartypants: false,
+          tables: true
+        }
+      },
+      pages: {
+        options: {
+          layout: 'component.hbs',
+          partials: '<%= config.src %>/app/styles/core/ks/*.less'
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/assemble/pages/{,*/}*.hbs']
+        }
+      }
+    },
+
+    // convert all CSV files into JSON files for Assemble
+    convert: {
+      csvs: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.src %>/<%= config.dataFolder %>/',
+            src: ['**/*.csv'],
+            dest: '<%= config.src %>/assemble/data/',
+            ext: '.json'
+          }
+        ]
+      }
+    }
+  });
+
+  // load assemble manually because it doesn't match the grunt-* pattern
+  grunt.loadNpmTasks('assemble');
+
+  // create server task
+  grunt.registerTask('serve', [
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('server', function () {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve']);
+  });
+
+  grunt.registerTask('build', [
+    'clean',
+    'convert',
+    'less',
+    'jshint',
+    'copy',
+    'assemble'
+  ]);
+
+  // set default grunt task
+  grunt.registerTask('default', [
+    'build'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,55 @@
+# propeller 0.5.6
+
+Static site generator in [Assemble](http://assemble.io) and [Bootstrap 3](http://getbootstrap.com) using [Handlebars](http://handlebarsjs.com) templates, with additional support for [RequireJS](http://requirejs.org), [Knockout](http://knockoutjs.com), [Moment.js](http://momentjs.com), [D3](http://d3js.org) and CSV parsing.
+
+## Getting started
+
+1. Download and install [Node](http://nodejs.org)
+1. At the command line install [Yeoman](http://yeoman.io), [Bower](http://bower.io) and [Grunt](http://gruntjs.com) globally by typing `sudo npm install -g yo bower grunt-cli`
+1. Clone, download and unzip, or use [generator-propeller](http://github.com/thegreatsunra/generator-propeller) to create a new Propeller project
+
+### Now you're ready to use Propeller
+
+1. Open your new Propeller project at the command line
+1. Type `npm install && bower install` to make sure all dependencies are installed
+1. Type `grunt` at the command line to run the tasks to generate your static site, which will appear in a `/dist` folder in your project
+1. Type `grunt serve` to start your development server. Propeller will monitor the files in the `/src` folder and update your static site whenever it detects changes
+1. Open [http://localhost:9000](http://localhost:9000) in a web browser to view your site
+1. Begin creating your site by modifying the files in the `/src` folder
+
+## Additional Information
+
+* Propeller uses [Assemble](http://assemble.io) and [Handlebars](http://handlebarsjs.com) as its templating engine
+* When the server is running it monitors for file updates, and automatically regenerates the static files
+* The server also monitors for changes to [LESS](http://lesscss.org) files, and automatically compiles them into static CSS files when updated
+
+## Resources
+* [propeller](http://propellerjs.com)
+* [assemble](http://assemble.io)
+* [yeoman](http://yeoman.io)
+* [bower](http://bower.io)
+* [grunt](http://gruntjs.com)
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Dane Petersen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
